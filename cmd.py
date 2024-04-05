9a6aac2881ce9b1f52be4f0bd74d4022b29ae7b2
Joseph P <s3ppzer0@gmail.com>
2024-04-05 17:33:57 +0000

    style: rename main function, change quotes and comments

--- cmd.py
+++ cmd.py
@@ -1,15 +1,15 @@
 import psycopg
 
 
-def launch(keyword: str) -> None:
-    '''Check the name provided from user.'''
-    with psycopg.connect(dbname='postgres', user='postgres') as connect:
+def main(keyword: str) -> None:
+    """Check the name provided from user."""
+    with psycopg.connect(dbname="postgres", user="postgres") as connect:
         with connect.cursor() as cur:
-            # this is a very unsafe way of passing a query to an adapter (psycopg),
-            # which enables the SQL injection
+            # NOTE: this is a very unsafe way of passing a query to an adapter (psycopg),
+            #       which enables the SQL injection
             query = "SELECT * FROM people WHERE \"person_name\" = '{}';"
             cur.execute(query.format(keyword))
-            # a much safer way would be (comment 2 lines above and uncomment 4 lines below):
+            # NOTE: a much safer way would be (comment 2 lines above and uncomment 4 lines below):
             #cur.execute(
             #    'SELECT * FROM people WHERE person_name = %s', 
             #    (keyword,)
@@ -18,11 +18,10 @@ def launch(keyword: str) -> None:
             if not result:
                 print("[\u2717] No data found on the provided name!")
             else:
-                for line in result:
-                    print(f"\nResponse: {line}\n")
+                [print(print(f"\nResponse: {line}\n")) for line in result]
                 print("[\u2713] Your request has been successfully processed!")
 
 
 # launch the demo
 if __name__ == "__main__":
-    launch(input('\n[?] Please, enter person\'s name: '))
+    main(input("\n[?] Please, enter person\'s name: "))
